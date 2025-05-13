23d7d20cbbc2a96ad3c1a0fde131cfbb9ff5fa82
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-13 15:29:32 +0200

    [UPD] Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+//importo axios
+import axios from 'axios'
 
 function App() {
+  const [actresses, setActresses] = useState([])
+
+  //creo una funzione per la chiamata ajax con axios
+  const fetchActresses = () => {
+    axios
+      .get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((response) => console.log(response.data))
+
+  }
+
+  useEffect(fetchActresses);
+
+
 
 
   return (
     <>
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-12">
+            <div className='d-flex justify-content-between'>
+              <h1>Actresses list</h1>
+            </div>
+          </div>
+        </div>
+      </div>
 
     </>
   )
