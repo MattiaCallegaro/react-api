8db9b31da75c271e013e5ba1501f72557372161b
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-13 16:31:16 +0200

    [FIX] Fix card

--- src/App.jsx
+++ src/App.jsx
@@ -22,32 +22,33 @@ function App() {
 
   return (
     <>
-      <div className="container mt-4">
-        <div className="row">
-          <div className="col-12">
-            <div className='d-flex justify-content-between'>
-              <h1>Actresses list</h1>
-            </div>
-          </div>
-          {actresses.map((actress) => (
-            <div className="col-12 col-md-6 col-lg-4"
-              key={`actresses-${actress.id}`}>
-              <div className="card mb-4">
-                <img src={actress.image} alt="" className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">{actress.name}</h5>
-                  <p className="card-text">{actress.birth_year}</p>
-                  <p className="card-text">{actress.nationality}</p>
-                  <p className="card-text">{actress.biography}</p>
-                  <p className="card-text">{actress.awards}</p>
-                </div>
+      <div className="bg-light min-vh-100">
+        <div className="container mt-4 ">
+          <div className="row ">
+            <div className="col-12">
+              <div className='d-flex justify-content-between'>
+                <h1>Actresses list</h1>
               </div>
-
             </div>
-          ))}
+            {actresses.map((actress) => (
+              <div className=" mb-4 col-12 col-md-6 col-lg-4"
+                key={`actresses-${actress.id}`}>
+                <div className="card  h-100 mt-4">
+                  <img src={actress.image} alt="" className="card-img-top" />
+                  <div className="card-body ">
+                    <h5 className="card-title">{actress.name}</h5>
+                    <p className="card-text">{actress.birth_year}</p>
+                    <p className="card-text">{actress.nationality}</p>
+                    <p className="card-text">{actress.biography}</p>
+                    <p className="card-text">{actress.awards}</p>
+                  </div>
+                </div>
+
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-
     </>
   )
 }
